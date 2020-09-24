9917dabe4e0e35db2a35ada8d359bdbf89aba4c2
arjunamayakara <52194004+arjunamayakara@users.noreply.github.com>
2020-09-24 16:40:30 +0530

    Add files via upload

---
+++ springboot-rest-example/springboot-rest-example/src/test/java/com/arjuna/springbootrestexample/SpringbootRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjuna.springbootrestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
